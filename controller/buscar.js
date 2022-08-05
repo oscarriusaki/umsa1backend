2ea964e1ee37b1b396266348804d08d00b54c240
oscar <oscarriusaki@gmail.com>
2022-08-04 18:16:06 -0700

    save

--- controller/buscar.js
+++ controller/buscar.js
@@ -24,6 +24,54 @@ const permitidos = [
     "buscarCompartirUsusriox1",
     "mostrarPublicacionesDelUsuarioX1",
 ];
+const enfermedadesV={
+    'ÁNTRAX':0,
+    'ASMA':0,
+    'AUTISMO':0,
+    'ARTRITIS':0,
+    'CÁNCER':0,
+    'CLAMIDIA':0,
+    'CULEBRILLA (HERPES ZÓSTER)':0,
+    'DÉFICIT DE ATENCIÓN E HIPERACTIVIDAD':0,
+    'DIABETES':0,
+    'ÉBOLA':0,
+    'EMBARAZO Y ETS':0,
+    'ENFERMEDADES DE TRANSMISIÓN SEXUAL (ETS)':0,
+    'ENFERMEDAD INFLAMATORIA PÉLVICA (EIP)':0,
+    'ENFERMEDAD PULMONAR OBSTRUCTIVA CRÓNICA':0,
+    'EPILEPSIA':0,
+    'ESCARLATINA':0,
+    'ESTREPTOCOCO DEL GRUPO B':0,
+    'GONORRHEA':0,
+    'HAEMOPHILUS INFLUENZAE TIPO B (HIB)':0,
+    'HEMOFILIA':0,
+    'HERPES GENITAL':0,
+    'INFECCÍON GENITAL POR VPH':0,
+    'INFLUENZA (GRIPE)':0,
+    'LA SALUD MENTAL DE LOS NIÑOS (CHILDREN’S MENTAL HEALTH)':0,
+    'LISTERIA (LISTERIOSIS)':0,
+    'MENINGITIS':0,
+    'NEUMONÍA':0,
+    'PAPERAS':0,
+    'POLIOMIELITIS':0,
+    'RABIA':0,
+    'REUMATISMO':0,
+    'ROTAVIRUS':0,
+    'SHIGELLA – SHIGELLOSIS':0,
+    'SÍFILIS':0,
+    'SILICOSIS':0,
+    'SÍNDROME ALCOHÓLICO FETAL':0,
+    'SÍNDROME DE FATIGA CRÓNICA (SFC)':0,
+    'SÍNDROME DE TOURETTE':0,
+    'TABAQUISMO':0,
+    'TOSFERINA':0,
+    'TRICOMONIASIS':0,
+    'TUBERCULOSIS (TB)':0,
+    'VAGINOSIS BACTERIANA':0,
+    'VIH/SIDA':0,
+    'ZIKA':0,
+    '#OTRO':0,
+  };
 
 const likePublicacion = async ( id, req, res=response) => {
     
@@ -1395,8 +1443,16 @@ const buscarContarPorEnfermedad = async (c,req,res) =>{
         { $group: { _id: "$tipoEnfermedad", total:{$sum:1} } },
         { $sort: { total:-1} }
     ])
+
+
+    data.map((i) =>{
+        console.log(i);
+        this.enfermedadesV[i._id] = i.total;
+      })
+
     res.json({
-        data
+        data,
+        enfermedadesV,
     })
 }
 const buscar = async( req, res= response) =>{
